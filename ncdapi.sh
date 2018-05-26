50170eb05186635de9cf12f844cbb81d03c0b01c
linux-insideDE <39219399+linux-insideDE@users.noreply.github.com>
2018-05-26 16:18:40 +0200

    local changes (Performance improvement, Bug Fixes)
    Restore Performance improvement up to 50%,
    hardening for some overflows,
    bug fixes

--- ncdapi.sh
+++ ncdapi.sh
@@ -6,157 +6,144 @@ apikey=YOUR_API_KEY
 apipw=YOUR_API_PASSWORD
 cid=YOUR_CUSTOMERNUMBER
 
-end=https://ccp.netcup.net/run/webservice/servers/endpoint.php?JSON
+end="https://ccp.netcup.net/run/webservice/servers/endpoint.php?JSON"
 client=""
 debug=false
 
 #Functions
 login() {
-	tmp=$(curl -s -X POST -d '{"action": "login", "param": {"apikey": "'$apikey'", "apipassword": "'$apipw'", "customernumber": "'$cid'"}}' $end)
-	sid=$(echo ${tmp} | jq -r .responsedata.apisessionid)
+	tmp=$(curl -s -X POST -d "{\"action\": \"login\", \"param\": {\"apikey\": \"$apikey\", \"apipassword\": \"$apipw\", \"customernumber\": \"$cid\"}}" "$end")
+	sid=$(echo "${tmp}" | jq -r .responsedata.apisessionid)
 	if [ $debug = true ]; then
-		msg=$(echo ${tmp} | jq -r .shortmessage)
-		echo $msg
-	fi
-	if [ "$(echo "$tmp" | jq -r .status)" != "success" ]; then
-		echo "Error: $tmp"
-		return 1
+		msg=$(echo "${tmp}" | jq -r .shortmessage)
+		echo "$msg"
 	fi
 }
 logout() {
-	tmp=$(curl -s -X POST -d '{"action": "logout", "param": {"apikey": "'$apikey'", "apisessionid": "'$sid'", "customernumber": "'$cid'"}}' $end)
+	tmp=$(curl -s -X POST -d "{\"action\": \"logout\", \"param\": {\"apikey\": \"$apikey\", \"apisessionid\": \"$sid\", \"customernumber\": \"$cid\"}}" "$end")
 	if [ $debug = true ]; then
-		msg=$(echo ${tmp} | jq -r .shortmessage)
-		echo $msg
-	fi
-	if [ "$(echo "$tmp" | jq -r .status)" != "success" ]; then
-		echo "Error: $tmp"
-		return 1
+		msg=$(echo "${tmp}" | jq -r .shortmessage)
+		echo "$msg"
 	fi
 }
 addRecord() {
 	login	
-	tmp=$(curl -s -X POST -d "{\"action\": \"updateDnsRecords\", \"param\": {\"apikey\": \"$apikey\", \"apisessionid\": \"$sid\", \"customernumber\": \"$cid\",\"clientrequestid\": \"$client\" , \"domainname\": \"$2\", \"dnsrecordset\": { \"dnsrecords\": [ {\"id\": \"\", \"hostname\": \"$1\", \"type\": \"$3\", \"priority\": \"${5:-"0"}\", \"destination\": \"$4\", \"deleterecord\": \"false\", \"state\": \"yes\"} ]}}}" $end)
+	tmp=$(curl -s -X POST -d "{\"action\": \"updateDnsRecords\", \"param\": {\"apikey\": \"$apikey\", \"apisessionid\": \"$sid\", \"customernumber\": \"$cid\",\"clientrequestid\": \"$client\" , \"domainname\": \"$2\", \"dnsrecordset\": { \"dnsrecords\": [ {\"id\": \"\", \"hostname\": \"$1\", \"type\": \"$3\", \"priority\": \"${5:-"0"}\", \"destination\": \"$4\", \"deleterecord\": \"false\", \"state\": \"yes\"} ]}}}" "$end")
 	if [ $debug = true ]; then
-		echo ${tmp}
-	fi
-	if [ "$(echo "$tmp" | jq -r .status)" != "success" ]; then
-		echo "Error: $tmp"
-		return 1
+		echo "${tmp}"
 	fi
 	logout
 }
 delRecord() {
 	login
-	tmp=$(curl -s -X POST -d "{\"action\": \"updateDnsRecords\", \"param\": {\"apikey\": \"$apikey\", \"apisessionid\": \"$sid\", \"customernumber\": \"$cid\",\"clientrequestid\": \"$client\" , \"domainname\": \"$3\", \"dnsrecordset\": { \"dnsrecords\": [ {\"id\": \"$1\", \"hostname\": \"$2\", \"type\": \"$4\", \"priority\": \"${6:-"0"}\", \"destination\": \"$5\", \"deleterecord\": \"TRUE\", \"state\": \"yes\"} ]}}}" $end)
+	tmp=$(curl -s -X POST -d "{\"action\": \"updateDnsRecords\", \"param\": {\"apikey\": \"$apikey\", \"apisessionid\": \"$sid\", \"customernumber\": \"$cid\",\"clientrequestid\": \"$client\" , \"domainname\": \"$3\", \"dnsrecordset\": { \"dnsrecords\": [ {\"id\": \"$1\", \"hostname\": \"$2\", \"type\": \"$4\", \"priority\": \"${6:-"0"}\", \"destination\": \"$5\", \"deleterecord\": \"TRUE\", \"state\": \"yes\"} ]}}}" "$end")
 	if [ $debug = true ]; then
-		echo ${tmp}
-	fi
-	if [ "$(echo "$tmp" | jq -r .status)" != "success" ]; then
-		echo "Error: $tmp"
-		return 1
+		echo "${tmp}"
 	fi
 	logout
 }
 modRecord() {
 	login	
-	tmp=$(curl -s -X POST -d "{\"action\": \"updateDnsRecords\", \"param\": {\"apikey\": \"$apikey\", \"apisessionid\": \"$sid\", \"customernumber\": \"$cid\",\"clientrequestid\": \"$client\" , \"domainname\": \"$3\", \"dnsrecordset\": { \"dnsrecords\": [ {\"id\": \"$1\", \"hostname\": \"$2\", \"type\": \"$4\", \"priority\": \"${6:-"0"}\", \"destination\": \"$5\", \"deleterecord\": \"false\", \"state\": \"yes\"} ]}}}" $end)
+	tmp=$(curl -s -X POST -d "{\"action\": \"updateDnsRecords\", \"param\": {\"apikey\": \"$apikey\", \"apisessionid\": \"$sid\", \"customernumber\": \"$cid\",\"clientrequestid\": \"$client\" , \"domainname\": \"$3\", \"dnsrecordset\": { \"dnsrecords\": [ {\"id\": \"$1\", \"hostname\": \"$2\", \"type\": \"$4\", \"priority\": \"${6:-"0"}\", \"destination\": \"$5\", \"deleterecord\": \"false\", \"state\": \"yes\"} ]}}}" "$end")
 	if [ $debug = true ]; then
-		echo ${tmp}
-	fi
-	if [ "$(echo "$tmp" | jq -r .status)" != "success" ]; then
-		echo "Error: $tmp"
-		return 1
+		echo "${tmp}"
 	fi
 	logout
 }
 getRecords() {
 	login	
-	tmp=$(curl -s -X POST -d "{\"action\": \"infoDnsRecords\", \"param\": {\"apikey\": \"$apikey\", \"apisessionid\": \"$sid\", \"customernumber\": \"$cid\", \"domainname\": \"$1\"}}" $end)
-	xxd=$(echo ${tmp} | jq -r '.responsedata.dnsrecords | .[]')
-	echo $xxd
+	tmp=$(curl -s -X POST -d "{\"action\": \"infoDnsRecords\", \"param\": {\"apikey\": \"$apikey\", \"apisessionid\": \"$sid\", \"customernumber\": \"$cid\", \"domainname\": \"$1\"}}" "$end")
+	if [ $debug = true ]; then
+		echo "${tmp}"
+	fi
+	xxd=$(echo "${tmp}" | jq -r '.responsedata.dnsrecords | .[]')
+	echo "$xxd"
 	logout
 }
 getRecordsONESESSION() {		
-	tmp=$(curl -s -X POST -d "{\"action\": \"infoDnsRecords\", \"param\": {\"apikey\": \"$apikey\", \"apisessionid\": \"$sid\", \"customernumber\": \"$cid\", \"domainname\": \"$1\"}}" $end)
-	xxd=$(echo ${tmp} | jq -r '.responsedata.dnsrecords | .[]')
-	echo $xxd	
+	tmp=$(curl -s -X POST -d "{\"action\": \"infoDnsRecords\", \"param\": {\"apikey\": \"$apikey\", \"apisessionid\": \"$sid\", \"customernumber\": \"$cid\", \"domainname\": \"$1\"}}" "$end")
+	xxd=$(echo "${tmp}" | jq -r '.responsedata.dnsrecords | .[]')
+	echo "$xxd"	
 }
 backup() {
 	debug=false
-	(getRecords $1 | sed 's/} {/},{/g') > backup-$1-$(date +%Y%m%d)-$(date +%H%M%S).txt
+	(getRecords "$1" | sed 's/} {/},{/g') > backup-"$1"-"$(date +%Y%m%d)"-"$(date +%H%M%S)".txt
 }
 restore() {
-	login	
-	bfile=$(cat $2)
-	bfile2=$(echo "[ $bfile ]")
-	currec=$(getRecordsONESESSION $1 | sed 's/} {/},{/g')
-	currec2=$(echo "[ $currec ]")	
-	
+	login
+	bfile=$(cat "$2")
+	bfile2="[ $bfile ]"
+	currec=$(getRecordsONESESSION "$1" | tr '\n' ' ' | sed "s/} {/},{/g")
+	currec2="[ $currec ]"
 	inc=0
 	#del all
-	len=$(echo $currec2 | jq '. | length')
+	len=$(echo "$currec2" | jq '. | length')
+	statement=""
 	while  [ "$inc" != "$len" ]  ; do
-		id=$(echo $currec2 | jq -r .[$inc].id)
-		host=$(echo $currec2 | jq -r .[$inc].hostname)
-		type=$(echo $currec2 | jq -r .[$inc].type)
-		prio=$(echo $currec2 | jq -r .[$inc].priority)
-		dest=$(echo $currec2 | jq -r .[$inc].destination)
-	    tmp=$(curl -s -X POST -d "{\"action\": \"updateDnsRecords\", \"param\": {\"apikey\": \"$apikey\", \"apisessionid\": \"$sid\", \"customernumber\": \"$cid\",\"clientrequestid\": \"$client\" , \"domainname\": \"$1\", \"dnsrecordset\": { \"dnsrecords\": [ {\"id\": \"$id\", \"hostname\": \"$host\", \"type\": \"$type\", \"priority\": \"$prio\", \"destination\": \"$dest\", \"deleterecord\": \"TRUE\", \"state\": \"yes\"} ]}}}" $end)
-		if [ $debug = true ]; then
-			echo ${tmp}
-		fi
-		if [ "$(echo "$tmp" | jq -r .status)" != "success" ]; then
-			echo "Error: $tmp"
-			return 1
-		fi
+		id=$(echo "$currec2" | jq -r .[$inc].id)
+		host=$(echo "$currec2" | jq -r .[$inc].hostname)
+		type=$(echo "$currec2" | jq -r .[$inc].type)
+		prio=$(echo "$currec2" | jq -r .[$inc].priority)
+		dest=$(echo "$currec2" | jq -r .[$inc].destination)
+		if [ "$inc" = "$((len-1))" ]; then
+			statement+="{\"id\": \"$id\", \"hostname\": \"$host\", \"type\": \"$type\", \"priority\": \"$prio\", \"destination\": \"$dest\", \"deleterecord\": \"TRUE\", \"state\": \"yes\"}"
+	    else
+			statement+="{\"id\": \"$id\", \"hostname\": \"$host\", \"type\": \"$type\", \"priority\": \"$prio\", \"destination\": \"$dest\", \"deleterecord\": \"TRUE\", \"state\": \"yes\"},"
+
+	    fi
 		inc=$((inc+1))
 	done
+	tmp=$(curl -s -X POST -d "{\"action\": \"updateDnsRecords\", \"param\": {\"apikey\": \"$apikey\", \"apisessionid\": \"$sid\", \"customernumber\": \"$cid\",\"clientrequestid\": \"$client\" , \"domainname\": \"$1\", \"dnsrecordset\": { \"dnsrecords\": [ $statement ]}}}" "$end")
+	if [ $debug = true ]; then
+		echo "${tmp}"
+	fi
 		
 	inc=0	
 	#add all
-	len=$(echo $bfile2 | jq '. | length')
+	statement=""
+	len=$(echo "$bfile2" | jq '. | length')
 	while [ "$inc" != "$len" ] ; do		
-		host=$(echo $bfile2 | jq -r .[$inc].hostname)
-		type=$(echo $bfile2 | jq -r .[$inc].type)
-		prio=$(echo $bfile2 | jq -r .[$inc].priority)
-		dest=$(echo $bfile2 | jq -r .[$inc].destination)
-	    tmp=$(curl -s -X POST -d "{\"action\": \"updateDnsRecords\", \"param\": {\"apikey\": \"$apikey\", \"apisessionid\": \"$sid\", \"customernumber\": \"$cid\",\"clientrequestid\": \"$client\" , \"domainname\": \"$1\", \"dnsrecordset\": { \"dnsrecords\": [ {\"id\": \"\", \"hostname\": \"$host\", \"type\": \"$type\", \"priority\": \"$prio\", \"destination\": \"$dest\", \"deleterecord\": \"false\", \"state\": \"yes\"} ]}}}" $end)
-		if [ $debug = true ]; then
-			echo ${tmp}
-		fi
-		if [ "$(echo "$tmp" | jq -r .status)" != "success" ]; then
-			echo "Error: $tmp"
-			return 1
-		fi
+		host=$(echo "$bfile2" | jq -r .[$inc].hostname)
+		type=$(echo "$bfile2" | jq -r .[$inc].type)
+		prio=$(echo "$bfile2" | jq -r .[$inc].priority)
+		dest=$(echo "$bfile2" | jq -r .[$inc].destination)		
+		if [ "$inc" = "$((len-1))" ]; then
+			statement+="{\"id\": \"\", \"hostname\": \"$host\", \"type\": \"$type\", \"priority\": \"$prio\", \"destination\": \"$dest\", \"deleterecord\": \"false\", \"state\": \"yes\"}"
+	    else
+			statement+="{\"id\": \"\", \"hostname\": \"$host\", \"type\": \"$type\", \"priority\": \"$prio\", \"destination\": \"$dest\", \"deleterecord\": \"false\", \"state\": \"yes\"},"
+	    fi	   
 		inc=$((inc+1))
-	done	
+	done
+	tmp=$(curl -s -X POST -d "{\"action\": \"updateDnsRecords\", \"param\": {\"apikey\": \"$apikey\", \"apisessionid\": \"$sid\", \"customernumber\": \"$cid\",\"clientrequestid\": \"$client\" , \"domainname\": \"$1\", \"dnsrecordset\": { \"dnsrecords\": [ $statement ]}}}" "$end")
+	if [ $debug = true ]; then
+		echo "${tmp}"
+	fi	
 	logout
 }
 install() {
-	nm=$(echo $0 | sed 's/\.\///g')
-	mkdir $(pwd)/api/
-	cp $0 $(pwd)/api/
-	chmod +x $(pwd)/api/$nm
+	nm=$(echo "$0" | sed 's/\.\///g')
+	mkdir "$(pwd)"/api/
+	cp "$0" "$(pwd)"/api/
+	chmod +x "$(pwd)"/api/"$nm"
 	st="alias ncdapi='$(pwd)/api/$nm'"
-	echo $st >> ~/.bashrc
+	echo "$st" >> ~/.bashrc
 }
 remove() {	
 	tmp=$(cat ~/.bashrc)
-	echo $tmp | sed "s/alias[:space:]ncdapi\=\'\$\(pwd\)\/api\/$nm\'//g" > ~/.bashrc
+	echo "$tmp" | sed "s/alias[:space:]ncdapi\\=\\'\$\\(pwd\\)\\/api\\/$nm\\'//g" > ~/.bashrc
 	
 }
 help() {
 	echo "IMPORTANT: Only ONE Argument like -N or -dN"
 	echo "If you have a string which is including spaces use \"around your string\""
 	echo ""
-	echo "-d   Debug Mode   ncdapi.sh -d..."
-	echo "-N   NEW Record	  ncdapi.sh -N HOST DOMAIN RECORDTYPE DESTINATION [PRIORITY]"
-	echo "-M   MOD Record	  ncdapi.sh -N ID HOST DOMAIN RECORDTYPE DESTINATION [PRIORITY]"
-	echo "-D   DEL Record	  ncdapi.sh -D ID HOST DOMAIN HOST DOMAIN RECORDTYPE DESTINATION [PRIORITY]"
-	echo "-g   get all Records	ncdapi.sh -g DOAMIN"
-	echo "-b   backup from Zone	ncdapi.sh -b DOMAIN"
-	echo "-R   Restore Zone	ncdapi.sh -R DOMAIN DATEI"
+	echo "-d   Debug Mode   ncapi.sh -d..."
+	echo "-N   NEW Record	  ncapi.sh -N HOST DOMAIN RECORDTYPE DESTINATION [PRIORITY]"
+	echo "-M   MOD Record	  ncapi.sh -N ID HOST DOMAIN RECORDTYPE DESTINATION [PRIORITY]"
+	echo "-D   DEL Record	  ncapi.sh -D ID HOST DOMAIN HOST DOMAIN RECORDTYPE DESTINATION [PRIORITY]"
+	echo "-g   get all Records	ncapi.sh -g DOAMIN"
+	echo "-b   backup from Zone	ncapi.sh -b DOMAIN"
+	echo "-R   Restore Zone	ncapi.sh -R DOMAIN DATEI"
 	echo "-I   Install Script"
 	echo "-h   this help"
 }
