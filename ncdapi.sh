368496d1802a7d6c1ae6dcdafd6cc38f49590cd6
linux-insideDE <39219399+linux-insideDE@users.noreply.github.com>
2018-06-05 19:08:32 +0200

    Support for SOA added + improved backup, restore
    new list all domains function, reduced active sessions, backup file is now in JSON

--- ncdapi.sh
+++ ncdapi.sh
@@ -20,7 +20,6 @@ login() {
 	fi
 	if [ "$(echo "$tmp" | jq -r .status)" != "success" ]; then
 		echo "Error: $tmp"
-		logout
 		return 1
 	fi
 }
@@ -31,6 +30,7 @@ logout() {
 		echo "$msg"
 	fi
 	if [ "$(echo "$tmp" | jq -r .status)" != "success" ]; then
+		echo "Error: Session isn't made invalid !!!"
 		echo "Error: $tmp"
 		return 1
 	fi
@@ -110,43 +110,115 @@ modRecord() {
 	fi
 	logout
 }
-getRecords() {
-	login	
-	tmp=$(curl -s -X POST -d "{\"action\": \"infoDnsRecords\", \"param\": {\"apikey\": \"$apikey\", \"apisessionid\": \"$sid\", \"customernumber\": \"$cid\", \"domainname\": \"$1\"}}" "$end")
+getSOA() {
+	login
+	tmp=$(curl -s -X POST -d "{\"action\": \"infoDnsZone\", \"param\": {\"apikey\": \"$apikey\", \"apisessionid\": \"$sid\", \"customernumber\": \"$cid\", \"domainname\": \"$1\"}}" "$end")
+	if [ $debug = true ]; then
+		echo "$tmp"
+	fi	
 	if [ "$(echo "$tmp" | jq -r .status)" != "success" ]; then
 		echo "Error: $tmp"
 		logout
 		return 1
 	fi
-	xxd=$(echo "${tmp}" | jq -r '.responsedata.dnsrecords | .[]')
+	xxd=$(echo "${tmp}" | jq -r '.responsedata')
+	echo "$xxd"
+	logout
+}
+getSOAONESESSION() {
+	tmp=$(curl -s -X POST -d "{\"action\": \"infoDnsZone\", \"param\": {\"apikey\": \"$apikey\", \"apisessionid\": \"$sid\", \"customernumber\": \"$cid\", \"domainname\": \"$1\"}}" "$end")
+	xxd=$(echo "${tmp}" | jq -r '.responsedata')
+	echo "$xxd"
+}
+setSOA() {
+	login
+	tmp=$(curl -s -X POST -d "{\"action\": \"updateDnsZone\", \"param\": {\"apikey\": \"$apikey\", \"apisessionid\": \"$sid\", \"customernumber\": \"$cid\",\"clientrequestid\": \"$client\" , \"domainname\": \"$1\", \"dnszone\": { \"name\": \"$1\", \"ttl\": \"$2\", \"serial\": \"\", \"refresh\": \"$3\", \"retry\": \"$4\", \"expire\": \"$5\", \"dnssecstatus\": \"$6\"} }}" "$end")
+	if [ $debug = true ]; then
+		echo "${tmp}"
+	fi
+	if [ "$(echo "$tmp" | jq -r .status)" != "success" ]; then
+		echo "Error: $tmp"
+		logout
+		return 1
+	fi
+	logout
+}
+listDomains() {
+	login	
+	tmp=$(curl -s -X POST -d "{\"action\": \"listallDomains\", \"param\": {\"apikey\": \"$apikey\", \"apisessionid\": \"$sid\", \"customernumber\": \"$cid\", \"domainname\": \"$1\"}}" "$end")
+	if [ $debug = true ]; then
+		echo "$tmp"
+	fi
+	if [ "$(echo "$tmp" | jq -r .status)" != "success" ]; then
+		echo "Error: $tmp"
+		logout
+		return 1
+	fi
+	xxd=$(echo "${tmp}" | jq -r '.responsedata[].domainname')
+	echo "$xxd"
+	logout
+}
+getRecords() {
+	login
+	tmp=$(curl -s -X POST -d "{\"action\": \"infoDnsRecords\", \"param\": {\"apikey\": \"$apikey\", \"apisessionid\": \"$sid\", \"customernumber\": \"$cid\", \"domainname\": \"$1\"}}" "$end")
+	if [ $debug = true ]; then
+		echo "$tmp"
+	fi
+	if [ "$(echo "$tmp" | jq -r .status)" != "success" ]; then
+		echo "Error: $tmp"
+		logout
+		return 1
+	fi
+	xxd=$(echo "${tmp}" | jq -r '.responsedata.dnsrecords')
 	echo "$xxd"
 	logout
 }
 getRecordsONESESSION() {		
 	tmp=$(curl -s -X POST -d "{\"action\": \"infoDnsRecords\", \"param\": {\"apikey\": \"$apikey\", \"apisessionid\": \"$sid\", \"customernumber\": \"$cid\", \"domainname\": \"$1\"}}" "$end")
-	xxd=$(echo "${tmp}" | jq -r '.responsedata.dnsrecords | .[]')
+	xxd=$(echo "$tmp" | jq -r '.responsedata')
 	echo "$xxd"	
 }
 backup() {
+	login
 	debug=false
-	(getRecords "$1" | sed 's/} {/},{/g') > backup-"$1"-"$(date +%Y%m%d)"-"$(date +%H%M%S)".txt
+	soa=$(getSOAONESESSION "$1")
+	records=$(getRecordsONESESSION "$1")
+	statement="{\"soa\":$soa,\"records\":$records}"
+	echo "$statement" > backup-"$1"-"$(date +%Y%m%d)"-"$(date +%H%M%S)".txt
+	logout
 }
 restore() {
 	login
-	bfile=$(cat "$2")
-	bfile2="[ $bfile ]"
-	currec=$(getRecordsONESESSION "$1" | tr '\n' ' ' | sed "s/} {/},{/g")
-	currec2="[ $currec ]"
+	bfile=$(cat "$1")
+	name=$(echo "$bfile" | jq -r '.soa.name')
+	ttl=$(echo "$bfile" | jq -r '.soa.ttl')
+	refresh=$(echo "$bfile" | jq -r '.soa.refresh')
+	retry=$(echo "$bfile" | jq -r '.soa.retry')
+	expire=$(echo "$bfile" | jq -r '.soa.expire')
+	dnssecstatus=$(echo "$bfile" | jq -r '.soa.dnssecstatus')
+	currec=$(getRecordsONESESSION "$name" | jq .dnsrecords)
 	inc=0
+	
+	#update soa	
+	tmp=$(curl -s -X POST -d "{\"action\": \"updateDnsZone\", \"param\": {\"apikey\": \"$apikey\", \"apisessionid\": \"$sid\", \"customernumber\": \"$cid\",\"clientrequestid\": \"$client\" , \"domainname\": \"$name\", \"dnszone\": { \"name\": \"$name\", \"ttl\": \"$ttl\", \"serial\": \"\", \"refresh\": \"$refresh\", \"retry\": \"$retry\", \"expire\": \"$expire\", \"dnssecstatus\": \"$dnssecstatus\"} }}" "$end")
+	if [ $debug = true ]; then
+		echo "${tmp}"
+	fi
+	if [ "$(echo "$tmp" | jq -r .status)" != "success" ]; then
+		echo "Error: $tmp"
+		logout
+		return 1
+	fi
+	
 	#del all
-	len=$(echo "$currec2" | jq '. | length')
+	len=$(echo "$currec" | jq '. | length')
 	statement=""
 	while  [ "$inc" != "$len" ]  ; do
-		id=$(echo "$currec2" | jq -r .[$inc].id)
-		host=$(echo "$currec2" | jq -r .[$inc].hostname)
-		type=$(echo "$currec2" | jq -r .[$inc].type)
-		prio=$(echo "$currec2" | jq -r .[$inc].priority)
-		dest=$(echo "$currec2" | jq -r .[$inc].destination)
+		id=$(echo "$currec" | jq -r .[$inc].id)
+		host=$(echo "$currec" | jq -r .[$inc].hostname)
+		type=$(echo "$currec" | jq -r .[$inc].type)
+		prio=$(echo "$currec" | jq -r .[$inc].priority)
+		dest=$(echo "$currec" | jq -r .[$inc].destination)
 		if [ "$inc" = "$((len-1))" ]; then
 			statement+="{\"id\": \"$id\", \"hostname\": \"$host\", \"type\": \"$type\", \"priority\": \"$prio\", \"destination\": \"$dest\", \"deleterecord\": \"TRUE\", \"state\": \"yes\"}"
 	    else
@@ -155,7 +227,7 @@ restore() {
 	    fi
 		inc=$((inc+1))
 	done
-	tmp=$(curl -s -X POST -d "{\"action\": \"updateDnsRecords\", \"param\": {\"apikey\": \"$apikey\", \"apisessionid\": \"$sid\", \"customernumber\": \"$cid\",\"clientrequestid\": \"$client\" , \"domainname\": \"$1\", \"dnsrecordset\": { \"dnsrecords\": [ $statement ]}}}" "$end")
+	tmp=$(curl -s -X POST -d "{\"action\": \"updateDnsRecords\", \"param\": {\"apikey\": \"$apikey\", \"apisessionid\": \"$sid\", \"customernumber\": \"$cid\",\"clientrequestid\": \"$client\" , \"domainname\": \"$name\", \"dnsrecordset\": { \"dnsrecords\": [ $statement ]}}}" "$end")
 	if [ $debug = true ]; then
 		echo "${tmp}"
 	fi
@@ -164,16 +236,16 @@ restore() {
 		logout
 		return 1
 	fi
-		
+	
 	inc=0	
 	#add all
 	statement=""
-	len=$(echo "$bfile2" | jq '. | length')
+	len=$(echo "$bfile" | jq '.records | length')
 	while [ "$inc" != "$len" ] ; do		
-		host=$(echo "$bfile2" | jq -r .[$inc].hostname)
-		type=$(echo "$bfile2" | jq -r .[$inc].type)
-		prio=$(echo "$bfile2" | jq -r .[$inc].priority)
-		dest=$(echo "$bfile2" | jq -r .[$inc].destination)		
+		host=$(echo "$bfile" | jq -r .records[$inc].hostname)
+		type=$(echo "$bfile" | jq -r .records[$inc].type)
+		prio=$(echo "$bfile" | jq -r .records[$inc].priority)
+		dest=$(echo "$bfile" | jq -r .records[$inc].destination)		
 		if [ "$inc" = "$((len-1))" ]; then
 			statement+="{\"id\": \"\", \"hostname\": \"$host\", \"type\": \"$type\", \"priority\": \"$prio\", \"destination\": \"$dest\", \"deleterecord\": \"false\", \"state\": \"yes\"}"
 	    else
@@ -181,7 +253,7 @@ restore() {
 	    fi	   
 		inc=$((inc+1))
 	done
-	tmp=$(curl -s -X POST -d "{\"action\": \"updateDnsRecords\", \"param\": {\"apikey\": \"$apikey\", \"apisessionid\": \"$sid\", \"customernumber\": \"$cid\",\"clientrequestid\": \"$client\" , \"domainname\": \"$1\", \"dnsrecordset\": { \"dnsrecords\": [ $statement ]}}}" "$end")
+	tmp=$(curl -s -X POST -d "{\"action\": \"updateDnsRecords\", \"param\": {\"apikey\": \"$apikey\", \"apisessionid\": \"$sid\", \"customernumber\": \"$cid\",\"clientrequestid\": \"$client\" , \"domainname\": \"$name\", \"dnsrecordset\": { \"dnsrecords\": [ $statement ]}}}" "$end")
 	if [ $debug = true ]; then
 		echo "${tmp}"
 	fi
@@ -189,7 +261,7 @@ restore() {
 		echo "Error: $tmp"
 		logout
 		return 1
-	fi
+	fi	
 	logout
 }
 help() {
@@ -202,18 +274,19 @@ help() {
 	echo "-D   DEL Record	  ncdapi.sh -D ID HOST DOMAIN HOST DOMAIN RECORDTYPE DESTINATION [PRIORITY]"
 	echo "-g   get all Records	ncdapi.sh -g DOAMIN"
 	echo "-b   backup from Zone	ncdapi.sh -b DOMAIN"
-	echo "-R   Restore Zone	ncdapi.sh -R DOMAIN DATEI"
-	echo "-I   Install Script"
+	echo "-R   Restore Zone	ncdapi.sh -R FILE"
+	echo "-s   get SOA    	ncdapi.sh -s DOAMIN"
+	echo "-S   change SOA    	ncdapi.sh -S DOAMIN TTL REFRESH RETRY EXPIRE DNSSECSTATUS"
+	echo "-l   list all Domains	ncdapi.sh -l"
 	echo "-h   this help"
 	echo ""
 	echo "Examples:"
-	echo "New CAA Record: ncdapi.sh -N @ example.com CAA \"0 issue letsencrypt.org\""
-	echo "New   A Record: ncdapi.sh -N @ example.com A 127.0.0.1"
-	echo "New  MX Record: ncdapi.sh -N @ example.com MX mail.example.com 20"
-	echo ""
+	echo "New CAA Record:  ncdapi.sh -N @ example.com CAA \"0 issue letsencrypt.org\""
+	echo "New   A Record:  ncdapi.sh -N @ example.com A 127.0.0.1"
+	echo "New  MX Record:  ncdapi.sh -N @ example.com MX mail.example.com 20"
 	echo "Get all records: ncdapi.sh -g example.com"
-	echo ""
-	echo "Delete Record:  ncdapi.sh -D 1234567 @ example.com A 127.0.0.1"
+	echo "Delete Record:   ncdapi.sh -D 1234567 @ example.com A 127.0.0.1"
+	echo "Change SOA:	 ncdapi.sh -S example.com 3600 28800 7200 1209600 true"
 }
 
 #begin script
@@ -223,7 +296,7 @@ if [ $# -eq 0 ]; then
 	help
 fi
 
-while getopts 'NdDgMbRIh' opt ; do
+while getopts 'NdDgMbRhslS' opt ; do
 	case "$opt" in
 		d) debug=true;;
 		N) addRecord "$2" "$3" "$4" "$5" "$6";;
@@ -231,8 +304,10 @@ while getopts 'NdDgMbRIh' opt ; do
 		g) getRecords "$2";;
 		M) modRecord "$2" "$3" "$4" "$5" "$6" "$7";;
 		b) backup "$2";;
-		R) restore "$2" "$3";;
-		I) install;;
+		R) restore "$2";;
+		s) getSOA "$2";;
+		S) setSOA "$2" "$3" "$4" "$5" "$6" "$7";;
+		l) listDomains "$2" ;;
 		h) help;;
 		*) echo "Invalid Argument";;
 	esac
